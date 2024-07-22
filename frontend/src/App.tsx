3c5f288d0562357e65ee44af057487a2de27916c
VamsiKrishna-7 <134033774+VamsiKrishna-7@users.noreply.github.com>
2024-07-22 16:42:46 +0530

    vite app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+  <div>
+    <center>
+      <h1>Hello world</h1>
+    </center>
+  </div>
+  )
+}
+
+export default App
